94d05cc24e54f81d898deddd25e010db983304fa
pnicto <f20210264@hyderabad.bits-pilani.ac.in>
2023-12-03 20:49:25 +0530

    day01: finish task 2

--- day01/main.go
+++ day01/main.go
@@ -6,59 +6,52 @@ import (
 	"os"
 	"regexp"
 	"strconv"
-
-	"github.com/pnicto/aoc/utils"
 )
 
-func replaceWords(wordToNumber map[string]int, line string) string {
-	line = regexp.MustCompile("twone").ReplaceAllString(line, "2ne")
-	line = regexp.MustCompile("nineight").ReplaceAllString(line, "9ight")
-	line = regexp.MustCompile("oneight").ReplaceAllString(line, "1ight")
-	line = regexp.MustCompile("fiveight").ReplaceAllString(line, "5ight")
-	line = regexp.MustCompile("threeight").ReplaceAllString(line, "3ight")
-	for word, number := range wordToNumber {
-		line = regexp.MustCompile(word).ReplaceAllString(line, strconv.Itoa(number))
-	}
+func replaceWords(line string) string {
+	line = regexp.MustCompile("twone").ReplaceAllString(line, "twoone")
+	line = regexp.MustCompile("oneight").ReplaceAllString(line, "oneeight")
+	line = regexp.MustCompile("threeight").ReplaceAllString(line, "threeeight")
+	line = regexp.MustCompile("fiveight").ReplaceAllString(line, "fiveeight")
+	line = regexp.MustCompile("eightwo").ReplaceAllString(line, "eighttwo")
+	line = regexp.MustCompile("eighthree").ReplaceAllString(line, "eightthree")
+
+	line = regexp.MustCompile("one").ReplaceAllString(line, "1")
+	line = regexp.MustCompile("two").ReplaceAllString(line, "2")
+	line = regexp.MustCompile("three").ReplaceAllString(line, "3")
+	line = regexp.MustCompile("four").ReplaceAllString(line, "4")
+	line = regexp.MustCompile("five").ReplaceAllString(line, "5")
+	line = regexp.MustCompile("six").ReplaceAllString(line, "6")
+	line = regexp.MustCompile("seven").ReplaceAllString(line, "7")
+	line = regexp.MustCompile("eight").ReplaceAllString(line, "8")
+	line = regexp.MustCompile("nine").ReplaceAllString(line, "9")
 	return line
 }
 
 func main() {
 	f, _ := os.Open("input.txt")
-	temp := utils.ReadInput("input.txt")
-	fmt.Println(temp)
 	defer f.Close()
-
-	wordToNumber := map[string]int{
-		"one":   1,
-		"two":   2,
-		"three": 3,
-		"four":  4,
-		"five":  5,
-		"six":   6,
-		"seven": 7,
-		"eight": 8,
-		"nine":  9,
-	}
 	sum := 0
 
 	re := regexp.MustCompile(`\d`)
 	scanner := bufio.NewScanner(f)
 	for scanner.Scan() {
-		line := replaceWords(wordToNumber, scanner.Text())
-		fmt.Println(scanner.Text())
-		fmt.Println(line)
+		// part 1
+		// line := scanner.Text()
+
+		// part 2
+		line := replaceWords(scanner.Text())
+
 		matches := re.FindAll([]byte(line), -1)
 		if matches != nil {
 			if len(matches) == 1 {
 				number, _ := strconv.Atoi(string(matches[0][0]))
 				sum += number*10 + number
-				fmt.Println(number*10 + number)
 			} else {
 				first, _ := strconv.Atoi(string(matches[0][0]))
 				second, _ := strconv.Atoi(string(matches[len(matches)-1][0]))
 				number := first*10 + second
 				sum += number
-				fmt.Println(number)
 			}
 		}
 	}
