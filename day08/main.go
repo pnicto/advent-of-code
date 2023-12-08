c25ca8dc0fead50bbc44d5ccdeb4261c3fbf4bb5
pnicto <f20210264@hyderabad.bits-pilani.ac.in>
2023-12-08 21:33:53 +0530

    day08: finish task 2

--- day08/main.go
+++ day08/main.go
@@ -51,7 +51,101 @@ func part1(contents string) int {
 	return traversePath(instructions, lookup)
 }
 
+// Brute force works for part 2, but is too slow
+func traversePathGhost(instruction string, lookup map[string]Node, startNodes []string) int {
+	const END = "Z"
+	current := startNodes
+
+	i := 0
+
+	for {
+		var newCurrent []string
+		for _, node := range current {
+			choice := string(instruction[i%len(instruction)])
+
+			if choice == "L" {
+				newCurrent = append(newCurrent, lookup[node].left)
+			} else {
+				newCurrent = append(newCurrent, lookup[node].right)
+			}
+		}
+		current = newCurrent
+
+		count := 0
+		for _, node := range newCurrent {
+			if strings.HasSuffix(node, END) {
+				count++
+			}
+		}
+
+		if count == len(startNodes) {
+			break
+		}
+		i++
+	}
+
+	return i + 1
+}
+
+func traversePathTest(instruction string, lookup map[string]Node, startNode string) int {
+	const END = "Z"
+	current := startNode
+	i := 0
+
+	for !strings.HasSuffix(current, END) {
+		choice := string(instruction[i%len(instruction)])
+
+		if choice == "L" {
+			current = lookup[current].left
+		} else {
+			current = lookup[current].right
+		}
+		i++
+	}
+	fmt.Println(current)
+	return i
+}
+
+func gcd(a, b int) int {
+	for b != 0 {
+		t := b
+		b = a % b
+		a = t
+	}
+	return a
+}
+
+func part2(contents string) int {
+	lookup := make(map[string]Node)
+
+	splits := strings.Split(contents, "\n\n")
+	instructions := splits[0]
+
+	splits = strings.Split(splits[1], "\n")
+
+	startNodes := []string{}
+
+	for _, split := range splits {
+		value, left, right := parseNode(split)
+		if strings.HasSuffix(value, "A") {
+			startNodes = append(startNodes, value)
+		}
+		lookup[value] = Node{left, right}
+	}
+
+	var nums []int
+
+	for _, startNode := range startNodes {
+		temp := traversePathTest(instructions, lookup, startNode)
+		nums = append(nums, temp)
+	}
+
+	fmt.Println(nums)
+
+	return 0
+}
+
 func main() {
 	contents := utils.ReadInput("input.txt")
-	fmt.Println(part1(contents))
+	fmt.Println(part2(contents))
 }
