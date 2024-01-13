a9b524ddf60e28e8370eca5c4a1a2a7312c7bc84
pnicto <f20210264@hyderabad.bits-pilani.ac.in>
2024-01-13 16:06:45 +0530

    day15: finish task 2

--- day15/main.go
+++ day15/main.go
@@ -30,7 +30,100 @@ func part1(contents string) int {
 	return res
 }
 
+type Data struct {
+	value     *int
+	label     string
+	operation string
+}
+
+type Lookup struct {
+	lookup map[int][]Data
+}
+
+func (l *Lookup) remove(boxNo int, label string) {
+	boxes := l.lookup[boxNo]
+	for i, box := range boxes {
+		if box.label == label {
+			boxes = append(boxes[:i], boxes[i+1:]...)
+			break
+		}
+	}
+
+	l.lookup[boxNo] = boxes
+}
+
+func (l *Lookup) add(boxNo int, data Data) {
+	boxes := l.lookup[boxNo]
+
+	found := false
+	for i, box := range boxes {
+		if box.label == data.label {
+			box.value = data.value
+			boxes[i] = box
+			found = true
+			break
+		}
+	}
+
+	if !found {
+		boxes = append(boxes, data)
+	}
+
+	l.lookup[boxNo] = boxes
+}
+
+func (l *Lookup) getFocusingPower() int {
+	focusingPower := 0
+
+	for boxNo, boxes := range l.lookup {
+		if len(boxes) > 0 {
+			for slotNo, box := range boxes {
+				focusingPower += (1 + boxNo) * (slotNo + 1) * (*box.value)
+			}
+		}
+	}
+
+	return focusingPower
+}
+
+func part2(contents string) int {
+	var l Lookup
+	l.lookup = make(map[int][]Data)
+
+	inputs := strings.Split(contents, ",")
+
+	for _, input := range inputs {
+		var data Data
+		if strings.Contains(input, "=") {
+			// additive
+			splits := strings.Split(input, "=")
+
+			data.label = splits[0]
+			data.operation = "="
+			data.value = new(int)
+			*data.value = utils.ToInt(splits[1])
+		} else {
+			// subtractive
+			splits := strings.Split(input, "-")
+			data.label = splits[0]
+			data.operation = "-"
+			data.value = nil
+		}
+
+		boxNo := hash(data.label)
+
+		if data.operation == "-" {
+			l.remove(boxNo, data.label)
+		} else {
+			l.add(boxNo, data)
+		}
+	}
+
+	return l.getFocusingPower()
+}
+
 func main() {
 	contents := utils.ReadInput("input.txt")
 	fmt.Println(part1(contents))
+	fmt.Println(part2(contents))
 }
