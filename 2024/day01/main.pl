10eab9ce273249d73f8cfe7d9da0be076e929d17
pnicto <f20210264@hyderabad.bits-pilani.ac.in>
2024-12-01 12:09:52 +0530

    day01: part 2

--- 2024/day01/main.pl
+++ 2024/day01/main.pl
@@ -5,6 +5,7 @@ use warnings;
 use Data::Dump qw(dump);
 use Path::Tiny;
 use List::MoreUtils qw(zip);
+use List::MoreUtils qw(frequency);
 use autodie;
 
 my $file = path(".")->child("input.txt");
@@ -30,5 +31,16 @@ foreach my $pairs ( @processed ) {
 my $sum = 0;
 foreach my($ele1, $ele2) (zip @list1, @list2) {
   $sum += abs($ele1 - $ele2);
+}
+print "$sum\n";
+
+# part 2
+$sum = 0;
+my %f = frequency(@list2);
+foreach my $ele1 (@list1) {
+  my $freq = $f{$ele1};
+  if ($freq) {
+    $sum += $ele1 * $freq;
+  }
 }
 print "$sum\n";
