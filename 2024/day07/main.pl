93b634cfd232a484c42d15c8d4c3caee462f1f99
pnicto <f20210264@hyderabad.bits-pilani.ac.in>
2024-12-07 11:16:43 +0530

    day07: part 2

--- 2024/day07/main.pl
+++ 2024/day07/main.pl
@@ -55,3 +55,44 @@ for my $test (@input) {
   }
 }
 say "Part 1: $part1";
+
+sub is_valid_t {
+  my ($target, $size, @nums) = @_;
+  return 1 if $size == 1 && $nums[0] == $target;
+  return check_target2($target, 0, 0, $size, @nums);
+}
+
+sub check_target2{
+  my ($target, $current_result, $index, $size, @nums) = @_;
+
+  if ($index == $size) {
+    return $current_result == $target;
+  }
+
+  # WHY???????
+  my $num = $nums[0][$index];
+  my $summ = 0;
+  my $multi = 0;
+  my $concat = 0;
+
+  $summ = (check_target2($target, $current_result + $num, $index+1, $size, @nums));
+  if ($index == 0) {
+    $multi = (check_target2($target, $num, $index + 1, $size, @nums));
+  } else {
+    $multi = (check_target2($target, $current_result * $num, $index + 1, $size, @nums));
+  }
+  $concat = (check_target2($target, $current_result . $num, $index + 1, $size, @nums));
+
+  return $summ == 1 || $multi == 1 || $concat == 1;
+}
+
+my $part2 = 0;
+for my $test (@input) {
+  my $target = $test->[0];
+  my $size = $test->[1];
+  my @nums = $test->[2];
+  if (is_valid_t($target, $size, @nums)) {
+    $part2 += $target;
+  }
+}
+say "Part 2: $part2";
