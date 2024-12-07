fd204445a38f468116addadd51fdcb1282b6ee66
pnicto <f20210264@hyderabad.bits-pilani.ac.in>
2024-12-07 23:49:51 +0530

    day07: use memoization
    this reduced the part2 rumtime to <10 seconds

--- 2024/day07/main.pl
+++ 2024/day07/main.pl
@@ -19,32 +19,42 @@ while (my ($index, $test) = each @input) {
 sub is_valid_test {
   my ($target, $nums, $is_part1) = @_;
   return 1 if @$nums == 1 && $nums->[0] == $target;
-  return check_target($target, 0, 0, $nums, $is_part1);
+
+  my %memo;
+  return check_target($target, 0, 0, $nums, $is_part1, \%memo);
 }
 
 sub check_target{
-  my ($target, $current_result, $index, $nums, $is_part1) = @_;
+  my ($target, $current_result, $index, $nums, $is_part1, $memo) = @_;
+
+  if ($current_result > $target) {
+    return 0;
+  }
 
   if ($index == @$nums) {
     return $current_result == $target;
   }
+
+  my $key = "$current_result,$index";
+  return $memo->{$key} if exists $memo->{$key};
 
   my $num = $nums->[$index];
   my $summ = 0;
   my $multi = 0;
   my $concat = 0;
 
-  $summ = check_target($target, $current_result + $num, $index+1, $nums, $is_part1);
+  $summ = check_target($target, $current_result + $num, $index+1, $nums, $is_part1, $memo);
   if ($index == 0) {
-    $multi = check_target($target, $num, $index + 1, $nums, $is_part1);
+    $multi = check_target($target, $num, $index + 1, $nums, $is_part1, $memo);
   } else {
-    $multi = check_target($target, $current_result * $num, $index + 1, $nums, $is_part1);
+    $multi = check_target($target, $current_result * $num, $index + 1, $nums, $is_part1, $memo);
   }
   if ($is_part1) {
-    $concat = check_target($target, $current_result . $num, $index + 1, $nums, $is_part1);
+    $concat = check_target($target, $current_result . $num, $index + 1, $nums, $is_part1, $memo);
   }
 
-  return $summ == 1 || $multi == 1 || $concat == 1;
+  $memo->{$key} = $summ || $multi || $concat;
+  return $memo->{$key};
 }
 
 my $part1 = 0;
