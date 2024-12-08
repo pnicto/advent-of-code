2ff638041f651e248feeda469e226badc725ceca
pnicto <f20210264@hyderabad.bits-pilani.ac.in>
2024-12-08 17:26:45 +0530

    update template with default file

--- 2024/template.pl
+++ 2024/template.pl
@@ -2,9 +2,14 @@ use 5.010;
 use strict;
 use warnings;
 
-use Data::Dump "dump";
-use Path::Tiny;
+use Data::Dump qw(dump);
+use Path::Tiny qw(path);
 use autodie;
 
-my $file  = $ARGV[0];
+my $file = $ARGV[0];
+if ( not defined $file ) {
+    say "Using default as sample.txt";
+    $file = "sample.txt";
+}
+
 my $input = path($file)->slurp;
